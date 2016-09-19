d13d732dae77478c6ecdd90427641baab91ba7e9
Blacard <Blacard@PC-201606062032>
2016-09-19 11:53:57 +0800

    add.将数字加单位形式的文件大小转换成纯数字

--- src/cn/blacard/nymph/String/NymFormat.java
+++ src/cn/blacard/nymph/String/NymFormat.java
@@ -47,4 +47,37 @@ public class NymFormat {
 		}
 		return length+fmt;
 	}
+	/**
+	 * 将以数字加单位形式的文件大小转换成具体数字
+	 * 例：“3.3MB” 转换为 “3460300” 
+	 * 目前仅支持 KB,MB,GB 单位的转换
+	 * @author Blacard
+	 * @create 2016年9月19日 上午11:42:32
+	 * @param str 数字加单位形式表示的文件大小
+	 * @return 转换后的纯数字文件大小
+	 */
+	public static long parseLong(String str){
+		//获取到参数的数字部分
+		float n = Float.parseFloat(str.substring(0,str.length()-2));
+		//获取到参数的单位部分
+		String unit = str.substring(str.length()-2,str.length());
+		//定义返回结果
+		long result = 0;
+		//将参数的数字部分转换成long类型，乘1000是防止丢失小数部分
+		long  num = (long)(n*1000);
+		switch(unit.toUpperCase()){
+		case "KB":
+			result = 1024*num;
+			break;
+		case "MB":
+			result = 1024*1024*num;
+			break;
+		case "GB":
+			result =  1024*1024*1024*num;
+			break;
+		}
+		//将上面的1000还回去
+		result = result/1000;
+		return result;
+	}
 }
