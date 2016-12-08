149d8720cac419c408a6221fc6cf94a74c2fafcd
blacard <blacard@163.com>
2016-12-08 14:03:51 +0800

    增加文件类型，即视频文件、音乐文件、或者图片文件

---
+++ file/cn/blacard/nymph/file/NymFileType.java
@@ -0,0 +1,7 @@
+package cn.blacard.nymph.file;
+
+public class NymFileType {
+	private static final String VIDEO = "video";
+	private static final String IMAGE = "image";
+	private static final String MUSIC = "music";
+}
