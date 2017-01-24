e24f0c9914fb5b55ea84f3d00a2d0b9619659738
blacard <blacard@163.com>
2017-01-24 16:54:49 +0800

    StringTool优化

--- src/cn/blacard/nymph/String/StringTool.java
+++ src/cn/blacard/nymph/String/StringTool.java
@@ -43,6 +43,25 @@ public class StringTool {
 			return list.get(0);
 		}
 	}
+	
+	/**
+	 * 去除字符中的所有空白符
+	 * 
+	 * @author Blacard
+	 * @create 2017年1月24日 下午2:49:23
+	 * @param str
+	 * @return
+	 */
+    public static String replaceBlank(String str) {
+        String dest = "";
+        if (str!=null) {
+            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            dest = m.replaceAll("");
+        }
+        return dest;
+    }
+	
 	/**
 	 * 获取后缀名
 	 * @author Blacard
