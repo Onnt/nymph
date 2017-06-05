7135357430449938c3bdd362b8e6369dab37d3cc
Blacard <blacard @163.com>
2017-06-05 11:28:49 +0800

    test

--- resource/cn/virde/test/Test.java
+++ resource/cn/virde/test/Test.java
@@ -1,16 +1,9 @@
 package cn.virde.test;
 
-import cn.virde.nymph.Nym;
-
 public class Test{
 	
 	public static void main(String[] args) throws Exception {
 		
-		String str = Nym.code.encode("{name:'user',pass:'pass'}", "sdfsd");
-		
-		System.out.println(str);
-		
-		System.out.println(Nym.code.decode(str));
 		
 	}
 	
