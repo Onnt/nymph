c1b97ed3fc8a3b459d72d79e1dbd3fe19d681f81
Blacard <blacard @163.com>
2017-06-29 20:21:17 +0800

    日志系统修改，可以设置输出模式

--- src/main/common/cn/virde/nymph/util/Log.java
+++ src/main/common/cn/virde/nymph/util/Log.java
@@ -3,17 +3,20 @@ package cn.virde.nymph.util;
 import java.util.Date;
 
 import cn.virde.nymph.Nym;
+import cn.virde.nymph.text.TextOut;
 
 public class Log {
 	//TODO 模式切换，日志文件输出模式 和 控制台输出模式 以及 双输出模式
+	private static String mode = "console" ;
+	private static String logFile ;
 	
 	public static void info(String msg){
 		
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
 		
-		System.out.println(time + " " + msg );
-		System.out.println(traceInfo);
+		syso(time + " " + msg );
+		syso(traceInfo);
 		
 	}
 	public static void info(String msg,Exception e){
@@ -21,8 +24,8 @@ public class Log {
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
 		
-		System.out.println(time + " " + msg + "。 异常信息："+e.getMessage());
-		System.out.println(traceInfo);
+		syso(time + " " + msg + "。 异常信息："+e.getMessage());
+		syso(traceInfo);
 		
 	}
 	
@@ -30,16 +33,30 @@ public class Log {
         StringBuffer sb = new StringBuffer();     
         sb.append("class: " ).append(stacks[1].getClassName()).append("; method: ").append(stacks[1].getMethodName()).append("; number: ").append(stacks[1].getLineNumber());
         return sb.toString();
-
+	}	
+	
+	public static void setFileOutMode(String file){
+		Log.logFile = file ;
+		Log.mode = "fileOut" ;
+	}
+	public static void setDoubleMode(String file){
+		Log.logFile = file ;
+		Log.mode = "console_and_file" ;
 	}
 	
-
-	//String _methodName =  
-	//  
-	//new Exception().getStackTrace()[1].getMethodName();// 获得调用者的方法名  
-	//  
-	//String _thisMethodName =  
-	//  
-	//new Exception().getStackTrace()[0].getMethodName();// 获得当前的方法名 
-	
+	private static void syso(String text){
+		switch(mode){
+		case "console" :
+			System.out.println(text);
+			break;
+		case "fileOut" :
+			new TextOut(logFile).putln(text);
+			break;
+		case "console_and_file" :
+			System.out.println(text);
+			new TextOut(logFile).putln(text);
+			break;
+		default : ;
+		}
+	}
 }
