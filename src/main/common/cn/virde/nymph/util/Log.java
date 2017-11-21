80d9114bf70c2a973840719aa89143399f4adaef
12w <12w@DESKTOP-RH7LP0P>
2017-11-21 18:13:39 +0800

    日志输出。优化为线程安全

--- src/main/common/cn/virde/nymph/util/Log.java
+++ src/main/common/cn/virde/nymph/util/Log.java
@@ -6,11 +6,12 @@ import cn.virde.nymph.Nym;
 import cn.virde.nymph.text.TextOut;
 
 public class Log {
-	//TODO 模式切换，日志文件输出模式 和 控制台输出模式 以及 双输出模式
+	// 模式切换，日志文件输出模式 和 控制台输出模式 以及 双输出模式
 	private static String mode = "console" ;
+	// 日志文件输出地址
 	private static String logFile ;
 	
-	public static void info(String msg){
+	public synchronized static void info(String msg){
 		
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
@@ -20,7 +21,7 @@ public class Log {
 		
 	}
 	
-	public static void info(String msg,Exception e){
+	public synchronized static void info(String msg,Exception e){
 		
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
@@ -30,7 +31,7 @@ public class Log {
 		
 	}
 
-	public static void info(String msg,String alert){
+	public synchronized static void info(String msg,String alert){
 		
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
@@ -41,7 +42,7 @@ public class Log {
 		
 	}
 
-	public static void error(String msg,Exception e){
+	public synchronized static void error(String msg,Exception e){
 		
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
@@ -52,7 +53,7 @@ public class Log {
 		
 	}
 
-	public static void error(String msg,String alert){
+	public synchronized static void error(String msg,String alert){
 		
         String traceInfo = getTraceInfo(new Throwable().getStackTrace());
 		String time = Nym.time.toString(new Date(), "hh:mm:ss");
@@ -63,22 +64,21 @@ public class Log {
 		syso(traceInfo);
 		syso("============================================");
 	}
+
+	public synchronized static void setFileOutMode(String file){
+		Log.logFile = file ;
+		Log.mode = "fileOut" ;
+	}
+	public synchronized static void setDoubleMode(String file){
+		Log.logFile = file ;
+		Log.mode = "console_and_file" ;
+	}
 	
 	private static String getTraceInfo(StackTraceElement[] stacks){
         StringBuffer sb = new StringBuffer();     
         sb.append("class: " ).append(stacks[1].getClassName()).append("; method: ").append(stacks[1].getMethodName()).append("; number: ").append(stacks[1].getLineNumber());
         return sb.toString();
 	}	
-	
-	public static void setFileOutMode(String file){
-		Log.logFile = file ;
-		Log.mode = "fileOut" ;
-	}
-	public static void setDoubleMode(String file){
-		Log.logFile = file ;
-		Log.mode = "console_and_file" ;
-	}
-	
 	private static void syso(String text){
 		switch(mode){
 		case "console" :
