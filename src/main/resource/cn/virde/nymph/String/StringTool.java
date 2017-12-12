9ff4d01bd7a5b2713982ae39b99ed59eefa1856c
Blacard <blacard@163.com>
2017-12-12 16:41:20 +0800

    Nym里面需要有一个获取Url参数的方法，有了

--- src/main/resource/cn/virde/nymph/String/StringTool.java
+++ src/main/resource/cn/virde/nymph/String/StringTool.java
@@ -1,7 +1,10 @@
 package cn.virde.nymph.String;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -107,4 +110,47 @@ public class StringTool {
 	    Matcher matcher = pattern.matcher(str);
 	    return matcher.matches();
     }
+    
+	/**
+	 * 返回url中名字为name的参数值。
+	 * 如果没有这个参数值或者发生异常，则返回NULL
+	 * 
+	 * @param url
+	 * @param name
+	 * @return
+	 */
+	public String getParam(String url,String name) {
+		Map<String,String> map = getParamMap(url);
+		Set<String> nameSet = map.keySet();
+		for(String n : nameSet) {
+			if(n.equals(name)) {
+				return map.get(n);
+			}
+		}
+		return null;
+	}
+	
+
+	/**
+	 * 返回pageUrl中携带的参数，存储到map中返回
+	 * @param pageUrl 带有参数的url地址
+	 * @return 
+	 *  如果不包含参数，或者遇到异常则返回空的Map对象
+	 * 
+	 */
+	public Map<String,String> getParamMap(String pageUrl){
+		Map<String,String> map = new HashMap<String,String>();
+		if(!pageUrl.contains("?")) return map;
+		
+		pageUrl = pageUrl.substring(pageUrl.indexOf("?")+1, pageUrl.length());
+		if(pageUrl.contains("#")) {
+			pageUrl = pageUrl.substring(0,pageUrl.indexOf("#"));
+		}
+		String[] paramKeyV = pageUrl.split("&");
+		for(String keyV : paramKeyV) {
+			String[] param = keyV.split("=");
+			map.put(param[0], param[1]);
+		}
+		return map ;
+	}
 }
