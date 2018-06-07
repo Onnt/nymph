4ef0d3460850bc0bbdd500691d38fd47c04ef29e
blacard <blacard@163.com>
2018-06-07 16:11:55 +0800

    http 模块升级至V2版本

--- src/main/resource/cn/virde/nymph/clazz/ClazzUtil.java
+++ src/main/resource/cn/virde/nymph/clazz/ClazzUtil.java
@@ -3,9 +3,12 @@ package cn.virde.nymph.clazz;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 import cn.virde.nymph.Nym;
 import cn.virde.nymph.common.info.RespInfo;
+import cn.virde.nymph.db.test.Person;
 import cn.virde.nymph.exception.NotExistFieldException;
 
 /**
@@ -37,6 +40,18 @@ public class ClazzUtil {
 		}
 		return null;
 	}
+	/**
+	 * 
+	 * @author Virde
+	 * @date 2018年6月7日 下午3:24:14
+	 * @param a
+	 * @param key
+	 * @return
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 * @throws NotExistFieldException
+	 */
 	public Object getField(Object a,String key) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NotExistFieldException {
 		Method[] aMethods = a.getClass().getMethods() ;
 		for(Method m : aMethods) {
@@ -47,4 +62,31 @@ public class ClazzUtil {
 		}
 		throw new NotExistFieldException("不存在的属性：" + key) ;
 	}
+	
+	/**
+	 * 
+	 * @author Virde
+	 * @date 2018年6月7日 下午3:24:08
+	 * @param a
+	 * @return
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 * @throws InvocationTargetException
+	 */
+	public Map<String,String> getField(Object a) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Map<String,String> respMap = new HashMap<String,String>();
+		Method[] aMethods = a.getClass().getMethods();
+		for(Method aMethod : aMethods) {
+			String aMethodName = aMethod.getName() ;
+			if(aMethodName.startsWith("get")&&!aMethodName.equals("getClass")) {
+				String key = aMethodName.substring(3).toLowerCase() ;
+				String value = (String) aMethod.invoke(a, null) ;
+				if(value != null && !value.equals("")) {
+					respMap.put(key, value);
+				}
+			}
+		}
+		return respMap ;
+	}
+	
 }
--- src/main/resource/cn/virde/nymph/net/v2/NymHttpPostRequest.java
+++ src/main/resource/cn/virde/nymph/net/v2/NymHttpPostRequest.java
@@ -1,10 +1,11 @@
-package cn.virde.nymph.net;
+package cn.virde.nymph.net.v2;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,11 +21,54 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
+import cn.virde.nymph.Nym;
 import cn.virde.nymph.exception.Not200Exception;
+import cn.virde.nymph.net.NymHttpGetHtml;
 import cn.virde.nymph.util.Log;
 
-public class NymHttp extends NymHttpGet{
-	
+/**
+ * 
+ * @author Virde
+ * @date 2018年6月7日 下午3:40:29
+ */
+public class NymHttpPostRequest extends NymHttpGetRequest{
+
+	/**
+	 * 
+	 * @author Virde
+	 * @date 2018年6月7日 下午3:44:13
+	 * @param url
+	 * @param params
+	 * @param clazz
+	 * @return
+	 * @throws NymHttpException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalArgumentException 
+	 * @throws IllegalAccessException 
+	 * @throws Not200Exception 
+	 * @throws IOException 
+	 */
+	public <T> T post(String url,Object params,Class<T> clazz) throws NymHttpException{
+		try {
+			Map<String, String> mapParams = Nym.clazz.getField(params);
+			String result = post(url, mapParams);
+			return Nym.json.jsonToObject(result, clazz);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			throw new NymHttpException("params error, detail info:"+e.getMessage()) ;
+		}catch (IOException | Not200Exception e) {
+			throw new NymHttpException("IO Exception, detail info:"+e.getMessage()) ;
+		}
+	}	
+	/**
+	 * 
+	 * @author Virde
+	 * @date 2018年6月7日 下午3:43:35
+	 * @param url
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 * @throws Not200Exception
+	 */
 	public String post(String url,List<NameValuePair> params) throws IOException, Not200Exception{
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 	    HttpPost httpPost = new HttpPost(url);
@@ -69,6 +113,7 @@ public class NymHttp extends NymHttpGet{
 		return new NymHttpGetHtml().getHtml(url);
 	}
 	
+
     private static String getStreamAsString(InputStream stream, String charset) throws IOException {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(stream, charset), 8192);
@@ -87,5 +132,4 @@ public class NymHttp extends NymHttpGet{
             }
         }
     }
-	
 }
