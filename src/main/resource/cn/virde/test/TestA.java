57e20c00407ef5e64fdb5b5d0a2183a31354a574
blacard <blacard@163.com>
2018-06-08 15:56:43 +0800

    增加AES128位加解密工具

--- src/main/resource/cn/virde/test/TestA.java
+++ src/main/resource/cn/virde/test/TestA.java
@@ -1,31 +1,13 @@
 package cn.virde.test;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import cn.virde.nymph.Nym;
+import cn.virde.nymph.code.AES;
 
 public class TestA{
 	public static void main(String[] args) {
-		System.out.println(new Sun().test().name);
-	}
-}
-
-class Person{
-	public String name = "f" ;
-	public Person test() {
-		return this ;
-	}
-}
-class Sun extends Person {
-
-	public String name = "s" ;
-	
-	@Override
-	public Sun test() {
-		super.test();
-		return this ;
+		String key = "3234sadf" ;
+		System.out.println(AES.encode("哈哈哈哈哈哈哈。asdf32342345《<>你是傻逼吧", key));
+		
+		System.out.println(AES.decode("493B3C7C5957687829B31862F0A50FEF1C017A1E77BF3FADFD1F79A56171B80D9A5EBD2CE4DF7885B90478450696770C7BF5378E41463EA4404FC929E462087F", key));
+		
 	}
 }
