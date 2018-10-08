510741be395e9a9d91483a68d0a12ed3bd6b1c33
Blacard <blacard@163.com>
2018-10-08 13:27:04 +0800

    完善邮件发送小工具

---
+++ src/main/java/cn/virde/nymph/mail/EmailClientInfo.java
@@ -0,0 +1,42 @@
+package cn.virde.nymph.mail;
+
+public class EmailClientInfo {
+
+	// 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
+	// 网易163邮箱的 SMTP 服务器地址为: smtp.163.com
+	private String smtpHost ;
+    // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）, 
+    //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
+	private String password ;
+
+	// 发件人地址
+	private String senderAddress ;
+	// 发件人名字
+	private String senderName ;
+	
+	public String getSmtpHost() {
+		return smtpHost;
+	}
+	public void setSmtpHost(String smtpHost) {
+		this.smtpHost = smtpHost;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSenderAddress() {
+		return senderAddress;
+	}
+	public void setSenderAddress(String senderAddress) {
+		this.senderAddress = senderAddress;
+	}
+	public String getSenderName() {
+		return senderName;
+	}
+	public void setSenderName(String senderName) {
+		this.senderName = senderName;
+	}
+	
+}
--- src/main/java/cn/virde/nymph/mail/MailInfo.java
+++ src/main/java/cn/virde/nymph/mail/MailInfo.java
@@ -1,23 +1,10 @@
 package cn.virde.nymph.mail;
 
 public class MailInfo {
-	// 发件人地址
-	private String senderAddress ;
-	// 发件人名字
-	private String senderName ;
-	// 发件人邮箱密码
-    // PS: 某些邮箱服务器为了增加邮箱本身密码的安全性，给 SMTP 客户端设置了独立密码（有的邮箱称为“授权码”）, 
-    //     对于开启了独立密码的邮箱, 这里的邮箱密码必需使用这个独立密码（授权码）。
-	private String password ;
 	// 收件人邮箱地址
 	private String recipientAddress ;
 	// 收件人名字
 	private String recipientName ;
-
-    // 发件人邮箱的 SMTP 服务器地址, 必须准确, 不同邮件服务器地址不同, 一般(只是一般, 绝非绝对)格式为: smtp.xxx.com
-    // 网易163邮箱的 SMTP 服务器地址为: smtp.163.com
-	private String smtpHost ;
-	
 
 	//  Subject: 邮件主题
 	private String subject ;
@@ -26,24 +13,6 @@ public class MailInfo {
 	// 附件地址
 	private String file ;
 	
-	public String getSenderAddress() {
-		return senderAddress;
-	}
-	public void setSenderAddress(String senderAddress) {
-		this.senderAddress = senderAddress;
-	}
-	public String getSenderName() {
-		return senderName;
-	}
-	public void setSenderName(String senderName) {
-		this.senderName = senderName;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 	public String getRecipientAddress() {
 		return recipientAddress;
 	}
@@ -55,18 +24,6 @@ public class MailInfo {
 	}
 	public void setRecipientName(String recipientName) {
 		this.recipientName = recipientName;
-	}
-	/**
-	 * 这个可以根据常见的邮箱地址自动获取
-	 * @author SunAo
-	 * 2018年4月13日 下午4:40:29
-	 * @return 返回
-	 */
-	public String getSmtpHost() {
-		return smtpHost;
-	}
-	public void setSmtpHost(String smtpHost) {
-		this.smtpHost = smtpHost;
 	}
 	public String getSubject() {
 		return subject;
@@ -86,7 +43,5 @@ public class MailInfo {
 	public void setFile(String file) {
 		this.file = file;
 	}
-	
-	// 抄送，多个收件人之类的后期再添加
 	
 }
