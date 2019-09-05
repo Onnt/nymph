e89f59a8e9f37c1300a04bc3f40a8c8113a5fab3
Blacard <blacard@163.com>
2019-09-05 17:17:36 +0800

    增加IPv4 IPv6地址的判断

--- src/main/java/cn/virde/nymph/util/IPUtils.java
+++ src/main/java/cn/virde/nymph/util/IPUtils.java
@@ -1,5 +1,8 @@
 package cn.virde.nymph.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author SunAo
  * @Date 2019/7/23
@@ -16,5 +19,28 @@ public class IPUtils {
     public static boolean isLocalIP(String ipAddr){
         String rexp = "(127\\.0\\.0\\.1)|(localhost)|(10\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3})|(172\\.((1[6-9])|(2\\d)|(3[01]))\\.\\d{1,3}\\.\\d{1,3})|(192\\.168\\.\\d{1,3}\\.\\d{1,3})";
         return ipAddr.matches(rexp);
+    }
+
+    public static boolean isIPV4(String addr) {
+        if (null == addr) {
+            return false;
+        } else {
+            String rexp = "^((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)$";
+            Pattern pat = Pattern.compile(rexp);
+            Matcher mat = pat.matcher(addr);
+            boolean ipAddress = mat.find();
+            return ipAddress;
+        }
+    }
+    public static boolean isIPV6(String addr) {
+        if (null == addr) {
+            return false;
+        } else {
+            String rexp = "^([\\da-fA-F]{1,4}:){7}[\\da-fA-F]{1,4}$";
+            Pattern pat = Pattern.compile(rexp);
+            Matcher mat = pat.matcher(addr);
+            boolean ipAddress = mat.find();
+            return ipAddress;
+        }
     }
 }
